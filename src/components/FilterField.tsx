95659921b0f5b4521ca0752df5572854dba47859
José Gabriel Mourão Bezerra <jotag.94@gmail.com>
2023-02-11 19:24:26 +0000

    feat: add search screen

---
+++ src/components/FilterField.tsx
@@ -0,0 +1,42 @@
+import { Checkbox, Text, VStack } from '@chakra-ui/react';
+
+type Option = {
+  id: string;
+  title: string;
+};
+
+type FilterFieldProps = {
+  title: string;
+  options: readonly Option[];
+  checkedOptionId?: string;
+  onChangeOption: (optionId?: string) => void;
+};
+
+export const FilterField = ({
+  title,
+  options,
+  checkedOptionId,
+  onChangeOption,
+}: FilterFieldProps) => (
+  <VStack spacing="20px" align="start">
+    <Text fontWeight={600} color="#9EAEB7" fontSize="14px">
+      {title}
+    </Text>
+
+    {options.map((option) => (
+      <Checkbox
+        key={`filter-option-${option.id}`}
+        colorScheme="gray"
+        borderColor="#053B4B"
+        isChecked={checkedOptionId === option.id}
+        onChange={(e) =>
+          onChangeOption(e.target.checked ? option.id : undefined)
+        }
+      >
+        <Text color="#053B4B" fontSize="14px">
+          {option.title}
+        </Text>
+      </Checkbox>
+    ))}
+  </VStack>
+);
