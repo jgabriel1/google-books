d3baf6bbf19efed8952577727164379c03c764c3
José Gabriel Mourão Bezerra <jotag.94@gmail.com>
2023-02-11 19:23:17 +0000

    feat: add providers to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { Inter } from '@next/font/google';
+import { Provider } from 'react-redux';
+import { Layout } from '../components/Layout';
+import { store } from '../store';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <main className={inter.className}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
+      </ChakraProvider>
+    </Provider>
+  );
 }
