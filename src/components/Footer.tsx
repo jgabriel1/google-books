ef9a5d94138482c0ec96d56add727918d0467199
jgabriel1 <jotag.94@gmail.com>
2023-02-12 11:36:18 -0300

    feat: add responsive styles to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,53 @@
-import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
-export const Footer = () => (
-  <Flex align="center" borderTop="0.5px solid #D9D9D9" marginTop="auto">
-    <Container maxW="1120px">
-      <Flex justify="space-between" align="center" p="28px">
-        <Text color="#B2B4B9" fontSize="13px" fontWeight={500}>
-          Copyright © 2021 Árvore. Todos os direitos reservados.
-        </Text>
+export const Footer = () => {
+  const isSmallestScreenSize = useBreakpointValue([true, false]);
+  const isScreenSmallOrWider = useBreakpointValue([true, true, false]);
 
-        <HStack spacing="12px">
-          <Button
-            variant="outline"
+  return (
+    <Flex align="center" borderTop="0.5px solid #D9D9D9" marginTop="auto">
+      <Container maxW="1120px">
+        <Flex justify={['center', 'space-between']} align="center" p="28px">
+          <Text
             color="#B2B4B9"
-            fontSize="16px"
+            fontSize="13px"
             fontWeight={500}
+            textAlign={['center', 'left']}
           >
-            Política de privacidade
-          </Button>
+            Copyright © 2021 Árvore. {isScreenSmallOrWider && <br />} Todos os
+            direitos reservados.
+          </Text>
 
-          <Button
-            variant="outline"
-            color="#B2B4B9"
-            fontSize="16px"
-            fontWeight={500}
-          >
-            Ajuda
-          </Button>
-        </HStack>
-      </Flex>
-    </Container>
-  </Flex>
-);
+          {!isSmallestScreenSize && (
+            <HStack spacing="12px">
+              <Button
+                variant="outline"
+                color="#B2B4B9"
+                fontSize="16px"
+                fontWeight={500}
+              >
+                Política de privacidade
+              </Button>
+
+              <Button
+                variant="outline"
+                color="#B2B4B9"
+                fontSize="16px"
+                fontWeight={500}
+              >
+                Ajuda
+              </Button>
+            </HStack>
+          )}
+        </Flex>
+      </Container>
+    </Flex>
+  );
+};
